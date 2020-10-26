8e9a18e55686db1df97554274860437a1a4e5ce5
András Ács <acsandras@gmail.com>
2020-10-26 13:19:15 +0100

    first commit

---
+++ test/converter.js
@@ -0,0 +1,15 @@
+var expect    = require("chai").expect;
+
+describe("Test-suite", function() {
+    describe("Dummy test PASS", function() {
+      it("Eksempel på PASS", function() {
+        expect(1+1).to.equal(2);
+      });
+    });
+
+    describe("Dummy test FAIL", function() {
+        it("Eksempel på FAIL", function() {
+            expect(1-1).to.equal(2);
+        });
+      });
+  });
